af22ead3df0c6742688806f2c7d111bf2af83782
Divyendra Patil <divyendrapatil@gmail.com>
2019-01-12 19:55:23 -0500

    Create shiftarray.java

---
+++ shiftarray.java
@@ -0,0 +1,8 @@
+        for(int i=0,j=k;j<a.length;i++,j++){
+            newar[j]=a[i];
+            ipointer++;
+        }
+
+        for (int i = 0; i < k; i++) {
+            newar[i] = a[ipointer++];
+        }
