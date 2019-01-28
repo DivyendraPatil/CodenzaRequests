04d7fbc6ca5ef9c23386f2220b4a9ff9d7cab111
Divyendra Patil <divyendrapatil@gmail.com>
2019-01-27 23:08:07 -0500

    Create First repeating character.java

---
+++ First repeating character.java
@@ -0,0 +1,25 @@
+import java.util.*; 
+  
+class Main {  
+	static char firstRepeating(char str[]) { 
+	
+		HashSet<Character> h = new HashSet<>(); 
+  
+		for (int i=0; i<=str.length-1; i++) { 
+			char c = str[i]; 
+  
+			if (h.contains(c)) 
+				return c; 
+  
+			else  
+				h.add(c); 
+		} 
+		return '\0'; 
+	} 
+  
+	public static void main (String[] args) { 
+		String str = "hello"; 
+		char[] arr = str.toCharArray(); 
+		System.out.println(firstRepeating(arr)); 
+	} 
+} 
