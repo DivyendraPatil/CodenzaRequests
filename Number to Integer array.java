8c121a7b4411710c9cf012515f03c2450eb57cf3
Divyendra Patil <divyendrapatil@gmail.com>
2019-01-27 23:16:55 -0500

    Create Number to Integer array.java

---
+++ Number to Integer array.java
@@ -0,0 +1,19 @@
+		int n = 46288;
+		//1
+		int[] newarray = Integer.toString(n).chars().map(c -> c-'0').toArray(); 
+		
+		//2
+		String temp = Integer.toString(n);
+		int[] newGuess = new int[temp.length()];
+		for (int i = 0; i < temp.length(); i++) {
+			newGuess[i] = temp.charAt(i) - '0';
+		}
+		
+		//3
+		int len = Integer.toString(n).length();
+		int[] arr = new int[len];
+		do { 
+		arr[i] = number % 10; 
+		number /= 10; 
+		i++; 
+		} while (number != 0); 
