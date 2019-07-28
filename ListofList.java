fa0d65b8ca06ac65f63ebac4e698b0de71ee7198
Divyendra Patil <divyendrapatil@gmail.com>
2019-07-28 11:17:56 -0400

    Trying to sort list of lists merging in less than n2

--- ListofList.java
+++ ListofList.java
@@ -24,16 +24,31 @@ class ListofList {
 		ls4.add(15);
 		ls4.add(16);
 		
-		ArrayList<ArrayList<Integer>> lsls = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> lsls1 = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> lsls2 = new ArrayList<ArrayList<Integer>>();
+	
+		lsls1.add(ls4);
+		lsls1.add(ls3);
+		lsls2.add(ls2);
+		lsls2.add(ls1);
 		
-		lsls.add(ls1);
-		lsls.add(ls2);
-		lsls.add(ls3);
-		lsls.add(ls4);
-		
-		for (int i = 0; i < lsls.size(); i++) {
-			System.out.println(lsls.get(i));
+		for (int i = 0; i < lsls1.size(); i++) {
+			System.out.println(lsls1.get(i));
+			System.out.println(lsls2.get(i));
 		}
 		
+		ArrayList<ArrayList<Integer>> lsls3 = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i < lsls1.size(); i++) {
+			ArrayList<Integer> ls5 = new ArrayList<Integer>();
+			ls5.addAll(lsls1.get(i));
+			ls5.addAll(lsls2.get(i));
+			lsls3.add(ls5);
+		}
+			
+		System.out.println(lsls3);
+		for (int i = 0; i < lsls3.size(); i++) {
+			Collections.sort(lsls3.get(i));
+		}
+		System.out.println(lsls3);
 	}
 }
